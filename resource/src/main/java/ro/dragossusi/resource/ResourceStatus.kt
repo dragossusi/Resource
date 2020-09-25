34ef92f05d22d126ce95d3b7635b33b05f5a615c
Dragos Rachieru <rachierudragos97@gmail.com>
2020-09-25 18:28:25 +0300

    Initial commit

---
+++ resource/src/main/java/ro/dragossusi/resource/ResourceStatus.kt
@@ -0,0 +1,10 @@
+package ro.dragossusi.resource
+
+/**
+ *
+ * @author Dragos
+ * @since 26.03.2020
+ */
+enum class ResourceStatus {
+    SUCCESS, ERROR, LOADING
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+include(":resource")
+rootProject.name = "Resource"
